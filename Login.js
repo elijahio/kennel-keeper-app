e4f62add7e884fc138e101c4a7bbda41c61d74d2
Meg O'Roark <msoroark@gmail.com>
2017-11-22 13:14:07 -0600

    banner component

---
+++ Login.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const navLinkStyle ={
+  float: 'right',
+};
+
+const Login = () =>
+
+  <div className="jumbotron">
+    <p>Kennel Keeper</p>
+  </div>
+
+
+ ;
+
+export default Nav;
--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Nav from "./components/Nav";
+import Banner from "./components/Banner";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
        <Nav />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Banner />
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
       </div>
     );
   }
