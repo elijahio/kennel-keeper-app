d09b0c905bccc2867444e6acd243265afdedf322
Elijah Kleinsmith <hi@elijah.io>
2017-11-27 20:52:05 -0600

    add basic page routing

---
+++ client/src/Pages/AdminLogin/AdminLogin.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class AdminLogin extends Component {
+
+
+	render(){
+		return(
+			 <div className="container-fluid">
+        		<div className="row">
+          			<div className="col-md-12">
+	          			<h1>Administrator Login</h1>
+          			</div>
+          		</div>
+          	</div>
+			);
+	}
+}
+export default AdminLogin;
