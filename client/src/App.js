8c1a8849ee094d171d054ac7dca97a460b781d99
Meg O'Roark <msoroark@gmail.com>
2017-11-27 20:56:56 -0600

    footer component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 // import Banner from "./components/Banner";
 import Landing from "./Pages/Landing";
 
@@ -15,6 +16,7 @@ class App extends Component {
           <p className="App-intro">
              To get started, edit <code>src/App.js</code> and save to reload.
           </p>
+        <Footer />
       </div>
     );
   }
