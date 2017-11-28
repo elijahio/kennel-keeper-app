103760dbadb8d71c6734df3e130a5b8b607c3ef3
Elijah Kleinsmith <hi@elijah.io>
2017-11-27 20:55:04 -0600

    heroku push

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 import Nav from "./components/Nav";
-// import Banner from "./components/Banner";
 import Landing from "./Pages/Landing";
 import AdminLogin from "./Pages/AdminLogin";
 
