71b33fcfcb2de3ce8daab8b58ea8e70a84fe566e
Jared <jmorgan4257@gmail.com>
2017-11-27 19:52:01 -0600

    added dogtask/controller in models and controller directories.

--- models/dogTasks.js
+++ models/dogTasks.js
@@ -1,1 +1,11 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const dogTaskSchema = new Schema({
+  taskname: { type: String, required: true },
+  status: { type: Boolean, default: false },
+});
+
+const dogTask = mongoose.model("dogTask", dogTaskSchema);
+
+module.exports = dogTask;
