d8eca89a45835d3afea5237303d439aa1e036d52
risaco <rhia.cooper@gmail.com>
2017-11-27 19:52:06 -0600

    updated general tasks model and controller

--- controllers/genTasksController.js
+++ controllers/genTasksController.js
@@ -1,1 +1,36 @@
 const db = require("../models");
+
+// Defining methods for the booksController
+module.exports = {
+  findAll: function(req, res) {
+    db.genTask
+      .find(req.query)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.genTask
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.genTask
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.genTask
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.genTask
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
--- models/generalTasks.js
+++ models/generalTasks.js
@@ -2,5 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const genTaskSchema = new Schema({
-
+  name: { type: String, required: true },
+  completed: { type: Boolean, default: false }
 });
+
+const genTask = mongoose.model("genTask", genTaskSchema);
+
+module.exports = genTask;
