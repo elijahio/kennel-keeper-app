95a54d9c2221e8eee99e70445c5d7d490300f4cf
Meg O'Roark <msoroark@gmail.com>
2017-11-27 21:29:36 -0600

    Footer component

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () =>
+
+	<footer className="navbar footer">
+		<div className="container-fluid footer">
+			<div className="footer-links">
+				<a href="#" className="small-links MIT">
+				MIT License &#x25CF;
+				</a>
+				<a href="#" className="small-links Privacy">
+				Privacy Policy &#x25CF;
+				</a>
+				<a href="#" className="small-links Volunteer">
+				Volunteer
+				</a>
+			</div>
+		</div>
+	</footer>
+;
+
+export default Footer;
