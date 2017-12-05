1288dc8986ba6e64937f5b2052671dd6f07eb4a7
risaco <rhia.cooper@gmail.com>
2017-12-04 19:58:59 -0600

    updated models and api routes for user and gen tasks

--- models/generalTasks.js
+++ models/generalTasks.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const genTaskSchema = new Schema({
   name: { type: String, required: true },
   completed: { type: Boolean, default: false }
-});
+},
+{ collection : 'genTask' });
 
 const genTask = mongoose.model("genTask", genTaskSchema);
 
